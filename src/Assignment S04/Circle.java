fee1c867b7be7c5fe03a2adc7e876efe94e717e8
Diego <diego@edu.uah.es>
2024-02-07 09:49:25 +0100

    .

--- src/Assignment S04/Circle.java
+++ src/Assignment S04/Circle.java
@@ -1,12 +1,24 @@
+
 public class Circle {
 
     private double radio;
     private String color;
-    public circle(){
+
+    public Circle (){
+        this.radio = 1.0;
+        this.color = "Azul";
+    }
+    public Circle(float radio, String color ) {
+        this.radio = radio;
+        this.color = color;
 
     }
 
+    public double getRadio() {
+        return this.radio;
+    }
 
-
-
+    public double getArea() {
+        return Math.PI*this.radio*this.radio;
+    }
 }
--- src/Assignment S04/Main.java
+++ src/Assignment S04/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+
+
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(3, "Azul");
+        System.out.println(c2.getArea());
+        System.out.println(c1.getRadio());
+    }
 }
